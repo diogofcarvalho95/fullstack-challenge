7342a33822cd4e1c16d1729c6c9764648f17438d
Diogo Carvalho <diogofcarvalho95@gmail.com>
2022-06-01 01:28:39 +0100

    aula2

---
+++ web/tailwind.config.js
@@ -0,0 +1,23 @@
+const brandColors = {
+  white: '#ffffff',
+  blue: '#1D9BF0',
+  platinium: '#E7E9EA',
+  silver: '#71767B',
+  onix: '#333639',
+  richBlack: '#15202B'
+}
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    colors: {
+      ... brandColors,
+      transparent: 'transparent',
+      current: 'currentColor',
+      backgroundColor: brandColors.richBlack,
+      textColor: brandColors.platinium
+    },
+    extend: {},
+  },
+  plugins: [],
+}
